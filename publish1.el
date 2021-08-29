65c3934e27906ad7753383f57c19f6ba2a9e86f1
kijima <norimaking777@gmail.com>
2021-08-30 01:32:12 +0900

    Adjust footer

--- publish1.el
+++ publish1.el
@@ -67,13 +67,15 @@
   (concat
    ;; "</div></div>"
    (sxml-to-xml
-    `(footer (@ (class "footer mt-auto py-3 fixed-bottom"))
+    `(footer (@ (class "footer mt-auto py-3"))
       (div (@ (class "container"))
            (div (@ (class "row"))
-                (div (@ (class "col-sm col-md text-md-right"))
-                     (nav (@ (class "navbar navbar-light"))
-                          ;; (a (@ (class "nav-link text-dark") (href "https://github.com/kijimaD")) "")
-                          ))))))
+                (div (@ (class "col-sm col-md"))
+                     (nav (@ (class "navbar navbar-light mr-auto"))
+                          (ul (@ (class "navbar-nav mr-auto"))
+                              (a (@ (class "nav-link text-dark") (href "https://github.com/kijimaD")) ""))
+                          (ul (@ (class "navbar-nav"))
+                              (a (@ (class "nav-link text-secondary small") (href "https://github.com/kijimaD")) "@kijimaD"))))))))
    (sxml-to-xml
     `(script (@ (src "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"))))))
 
