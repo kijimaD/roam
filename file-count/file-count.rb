1ef62c6775cfa690b79a446bea67a36863b48714
kijima <norimaking777@gmail.com>
2021-12-25 18:51:05 +0900

    refactor: add link to file count table

--- file-count/file-count.rb
+++ file-count/file-count.rb
@@ -1,15 +1,18 @@
 require 'open3'
 
-rows = Open3.capture2('git ls-files *.org | xargs wc -c | sort -n').first.split("\n")
-sum = rows.last.split(' ').first.to_f
+rows = Open3.capture2('git ls-files *.org | xargs wc -c | sort -nr').first.split("\n")
+sum = rows.first.split(' ').first.to_f
+rows.shift # remove sum
 
 puts '* File ranking'
-puts '| Character | Title | Percent |'
+puts '| Char Count | Title | Percent |'
 
 rows.each do |row|
-  count, title = row.strip.split(' ')
+  count, file = row.strip.split(' ')
   percent = ((count.to_i / sum).round(4) * 100).round(2)
-  title = title.gsub(/[0-9]{14}-/, '')
 
-  puts "| #{count} | #{title} | #{percent} |"
+  html = file.gsub(/\.org$/, "\.html")
+  title = file.gsub(/[0-9]{14}-/, "").gsub(/\.org$/, "")
+
+  puts "| #{count} | [[file:#{html}][#{title}]] | #{percent} |"
 end
