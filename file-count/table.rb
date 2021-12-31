0e5a79a650753d6d6d846bc7f05a27c9294f6669
kijimaD <norimaking777@gmail.com>
2021-12-31 21:20:48 +0900

    feat: file analysis

--- file-count/table.rb
+++ file-count/table.rb
@@ -15,6 +15,31 @@ class Table
     end
 
     results = results.sort { |(k1, v1), (k2, v2)| v2[:rank] <=> v1[:rank] }
+    print(results)
+  end
+
+  def print(results)
+    puts '* Node Analysis'
+    puts '| Page Rank | Title | Char Count | Volume | Last Changed |'
+
+    results.each do |result|
+      file = result[0]
+
+      title = result[1][:title]
+      rank = result[1][:rank]
+      count = result[1][:count]
+      percent = result[1][:percent]
+      last_changed = result[1][:last_changed]
+        text = <<~TEXT
+                | #{rank} \
+                | [[file:#{file}][#{title}]] \
+                | #{count} \
+                | #{percent} \
+                | #{last_changed} \
+                |
+              TEXT
+        puts text.gsub(/(\n)/,'')
+    end
   end
 end
 
