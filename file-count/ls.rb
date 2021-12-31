47845288112db5b58656cf2a142c8bda89b6d154
kijimaD <norimaking777@gmail.com>
2021-12-31 23:29:20 +0900

    refactor: xargs warning

--- file-count/ls.rb
+++ file-count/ls.rb
@@ -42,7 +42,7 @@ class Ls
   def last_changed_run
     results = Hash.new
 
-    rows = Open3.capture2('git ls-files -z *.org | xargs -0 -n1 -I{} -- git log -1 --format="%ai {}" {}').first.split("\n")
+    rows = Open3.capture2('git ls-files -z *.org | xargs -0 -I{} -- git log -1 --format="%ai {}" {}').first.split("\n")
     rows.each do |row|
       date = row.split(" ")[0]
       file = row.split(" ")[3]
