1f435fe44f6f323dfe0f7263ea95326ed778bce5
kijima <norimaking777@gmail.com>
2022-01-07 22:44:00 +0900

    feat: set link style

--- publish.el
+++ publish.el
@@ -96,6 +96,7 @@
 (setq my-blog-extra-head
       (concat
        "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' />"
+       "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'>"
        "<link rel='stylesheet' href='css/site.css' />"
        "<link rel='stylesheet' href='css/code.css' />"))
 
