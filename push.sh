9acf33cd8570460a8cbc8b1ed18eb27fc28d1025
kijima <norimaking777@gmail.com>
2022-02-06 23:38:31 +0900

    refactor: add to script

--- push.sh
+++ push.sh
@@ -4,3 +4,9 @@ LC=$(git rev-parse --short HEAD)
 docker build --target build -t ghcr.io/kijimad/roam:${LC} -t ghcr.io/kijimad/roam:master .
 docker push ghcr.io/kijimad/roam:master
 docker push ghcr.io/kijimad/roam:${LC}
+
+docker build --target ruby -t ghcr.io/kijimad/roam_ruby:master .
+docker push ghcr.io/kijimad/roam_ruby:master
+
+docker build --target lint -t ghcr.io/kijimad/roam_lint:master .
+docker push ghcr.io/kijimad/roam_lint:master
