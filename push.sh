5b07590c2869a91255838b8887d6a39007e0e430
kijima <norimaking777@gmail.com>
2022-02-08 00:27:20 +0900

    build: use image

--- push.sh
+++ push.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
 LC=$(git rev-parse --short HEAD)
+
+docker build --target ruby -t ghcr.io/kijimad/roam_ruby:master .
+docker push ghcr.io/kijimad/roam_ruby:master
+
 docker build --target build -t ghcr.io/kijimad/roam:${LC} -t ghcr.io/kijimad/roam:master .
 docker push ghcr.io/kijimad/roam:master
 docker push ghcr.io/kijimad/roam:${LC}
 
-docker build --target ruby -t ghcr.io/kijimad/roam_ruby:master .
-docker push ghcr.io/kijimad/roam_ruby:master
+docker build --target release -t ghcr.io/kijimad/roam_release:${LC} -t ghcr.io/kijimad/roam_release:master .
+docker push ghcr.io/kijimad/roam_release:master
+docker push ghcr.io/kijimad/roam_release:${LC}
 
 docker build --target lint -t ghcr.io/kijimad/roam_lint:master .
 docker push ghcr.io/kijimad/roam_lint:master
