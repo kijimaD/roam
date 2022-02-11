1ca02aa3319594144d0cd29ea1b6a78a8a07f67f
kijima <norimaking777@gmail.com>
2022-02-11 20:30:54 +0900

    build: nasty work

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git add .
+git commit -m 'chore: auto'
+
 make file-graph && \
     make line-graph && \
     make update-index && \
