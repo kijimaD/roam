46189434c1bb52980249810df095a6cf74a54010
kijimaD <norimaking777@gmail.com>
2022-05-06 00:19:54 +0900

    feat: adjust style

--- publish.el
+++ publish.el
@@ -118,7 +118,10 @@
 (setq my-blog-extra-head
       (concat
        "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' />"
-       "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'>"
+       "<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">"
+       "<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>"
+       "<link href=\"https://fonts.googleapis.com/css2?family=Ultra&display=swap\" rel=\"stylesheet\">"
+
        "<link rel='stylesheet' href='css/site.css' />"
        "<link rel='stylesheet' href='css/code.css' />"))
 
