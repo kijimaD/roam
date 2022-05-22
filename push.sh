47edec33e331951f82fd18393c998867ea5a60ec
kijimaD <norimaking777@gmail.com>
2022-05-22 23:55:30 +0900

    build: use buildx

--- push.sh
+++ push.sh
@@ -2,16 +2,16 @@
 
 LC=$(git rev-parse --short HEAD)
 
-docker build --target ruby -t ghcr.io/kijimad/roam_ruby:master --build-arg BUILDKIT_INLINE_CACHE=1 .
+docker buildx --target ruby -t ghcr.io/kijimad/roam_ruby:master --build-arg BUILDKIT_INLINE_CACHE=1 .
 docker push ghcr.io/kijimad/roam_ruby:master
 
-docker build --target build -t ghcr.io/kijimad/roam_build:${LC} -t ghcr.io/kijimad/roam_build:master --build-arg BUILDKIT_INLINE_CACHE=1 .
+docker buildx --target build -t ghcr.io/kijimad/roam_build:${LC} -t ghcr.io/kijimad/roam_build:master --build-arg BUILDKIT_INLINE_CACHE=1 .
 docker push ghcr.io/kijimad/roam_build:master
 docker push ghcr.io/kijimad/roam_build:${LC}
 
-docker build --target release -t ghcr.io/kijimad/roam_release:${LC} -t ghcr.io/kijimad/roam_release:master --build-arg BUILDKIT_INLINE_CACHE=1 .
+docker buildx --target release -t ghcr.io/kijimad/roam_release:${LC} -t ghcr.io/kijimad/roam_release:master --build-arg BUILDKIT_INLINE_CACHE=1 .
 docker push ghcr.io/kijimad/roam_release:master
 docker push ghcr.io/kijimad/roam_release:${LC}
 
-docker build --target lint -t ghcr.io/kijimad/roam_lint:master --build-arg BUILDKIT_INLINE_CACHE=1 .
+docker buildx --target lint -t ghcr.io/kijimad/roam_lint:master --build-arg BUILDKIT_INLINE_CACHE=1 .
 docker push ghcr.io/kijimad/roam_lint:master
