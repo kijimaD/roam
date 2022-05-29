c8da42f91a8b9391bb46ce57ae2969534ab2a597
kijimaD <norimaking777@gmail.com>
2022-05-29 07:16:08 +0000

    Deploying to gh-pages from @ kijimaD/roam@13f2c73e00cf08de32bd13a32b73d0d8d680c8ea 🚀

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-make file-graph && \
-    make line-graph && \
-    make update-index && \
-    make node-graph && \
-    make pmd-graph && \
-    make gen-file-table && \
-    make org2html
