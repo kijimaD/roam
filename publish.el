21bac9328138e369e9ea1e2a0c22725fa45fa1e2
Kijima Daigo <norimaking777@gmail.com>
2023-10-09 12:44:00 +0900

    fix: display favicon on GitHub Pages

--- publish.el
+++ publish.el
@@ -121,8 +121,13 @@
 
 (setq my-blog-extra-head
       (concat
+       ;; ブラウザは、そのサイトのホスト直下に favicon.ico が 設置されていることを期待する。
+       ;; GitHub Pagesにデプロイすると、ベースURLがプロジェクト下になるのでホスト直下にfaviconが配置されていないことになる。
+       ;; なので明示的に指定してやる。
+       "<link rel='shortcut icon' type='image/x-icon' href='/roam/favicon.ico' />"
+
        "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' />"
-       "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'>"
+       "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css' />"
 
        ;; ルートディレクトリにないorgファイルをエクスポートした場合、相対パスが変わる。絶対パスにするとGH pagesのルートディレクトリがpublicの一つ上の階層になる(/リポジトリ名/public)ため、ローカル環境で使えなくなる。つまりローカルでは`/`でokなのに、本番では`/roam`としないといけない。仕方ないので両方読み込む
        "<link rel='stylesheet' href='../css/site.css' />"
