bcb9d38b287526bf73ee3a168d9bbbd6e373aa7e
Kijima Daigo <norimaking777@gmail.com>
2024-02-07 01:05:16 +0900

    fix: deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eux
 
-make file-graph line-graph update-index node-graph pmd-graph gen-file-table make org2html
+make file-graph line-graph update-index node-graph pmd-graph gen-file-table org2html
