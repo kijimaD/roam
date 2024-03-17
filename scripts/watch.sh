ce14b174931617aaa9bc73f8aa3c6241b455c571
Kijima Daigo <norimaking777@gmail.com>
2024-03-17 19:30:59 +0900

    docs: publish

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -12,21 +12,35 @@ which inotifywait # 依存チェック
 
 docker build . --target textlint -t roam_textlint
 
-inotifywait -m -e modify --format '%w%f' . | while read FILE; do
-  if [[ $FILE =~ .*org$ ]]; then
-      echo "File $FILE was modified..."
+modify() {
+    inotifywait -m -e modify --format '%w%f' . | while read FILE; do
+        if [[ $FILE =~ .*org$ ]]; then
+            echo "File $FILE was modified..."
 
-      emacs --batch -l ./publish.el \
-            --eval "(require 'org)" \
-            --eval "(find-file \"./$FILE\")" \
-            --eval "(org-publish-current-file)"
-      if [ $? -gt 0 ]; then
-          notify-send "Fail Build" ""
-      fi
+            emacs --batch -l ./publish.el \
+                  --eval "(require 'org)" \
+                  --eval "(find-file \"./$FILE\")" \
+                  --eval "(org-publish-current-file)"
+            if [ $? -gt 0 ]; then
+                notify-send "Fail Build" ""
+            fi
 
-      docker run -v $PWD:/work -w /work --rm roam_textlint npx textlint -c ./.textlintrc $FILE
-      if [ $? -gt 0 ]; then
-          notify-send "Fail Textlint" ""
-      fi
-  fi
-done
+            docker run -v $PWD:/work -w /work --rm roam_textlint npx textlint -c ./.textlintrc $FILE
+            if [ $? -gt 0 ]; then
+                notify-send "Fail Textlint" ""
+            fi
+        fi
+    done
+}
+
+refresh() {
+    inotifywait -m -e create -e delete --format '%w%f' . | while read FILE; do
+        echo "File $FILE was create or delete or rename..."
+        # sitemapを更新する
+        make org2html
+    done
+}
+
+modify &
+refresh &
+wait
