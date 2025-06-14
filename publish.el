8c32ec7de0913a147c065d3655ab6f9969366b37
Kijima Daigo <norimaking777@gmail.com>
2025-06-14 17:44:57 +0900

    chore: remove entry count limit

--- publish.el
+++ publish.el
@@ -283,8 +283,7 @@
    :builder 'webfeeder-make-atom
    :title "Insomnia"
    :description "Insomnia"
-   :author "Kijima Daigo"
-   :max-entries 100))
+   :author "Kijima Daigo"))
 
 ;; バックリンクをつける
 ;; https://www.takeokunn.org/posts/permanent/20231219122351-how_to_manage_blog_by_org_roam/
