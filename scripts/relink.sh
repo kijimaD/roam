5b179f92c3ba34abdd6de0a157b67841203f1fa8
Kijima Daigo <norimaking777@gmail.com>
2025-06-27 08:05:50 +0900

    chore: タイトル書き換えスクリプトを追加した

---
+++ scripts/relink.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -eux
+
+##############################
+# サイト内リンクのタイトルを更新する
+##############################
+
+cd `dirname $0`
+cd ..
+
+TARGET_FILE="$1"
+
+if [[ -z "$TARGET_FILE" ]]; then
+  echo "使い方: $0 <IDとタイトルを含むファイル>"
+  exit 1
+fi
+
+ID="$(grep '^:ID:' "$TARGET_FILE" | awk '{print $2}')"
+TITLE=$(grep '^#+title:' "$TARGET_FILE" | sed -n 's/^#+title:[ \t]*\(KDOC.*\)/\1/p')
+
+if [[ -z "$ID" || -z "$TITLE" ]]; then
+  echo "ID または TITLE が見つかりません: $TARGET_FILE"
+  exit 1
+fi
+
+# すべての org ファイルでリンク置換を実行する
+find . -name '*.org' | while read LINK_FILE; do
+  # 対象ファイル自身はスキップ
+  if [[ "$LINK_FILE" == "$TARGET_FILE" ]]; then
+    continue
+  fi
+
+  TMP_FILE=$(mktemp)
+  # TODO: マッチしたときだけ書かせたい
+  sed -E "s|\[\[id:$ID\]\[.*\]\]|[[id:$ID][$TITLE]]|g" $LINK_FILE > "$TMP_FILE"
+  mv "$TMP_FILE" "$LINK_FILE"
+  echo "置換完了: $LINK_FILE の id:${ID} リンクタイトルを更新"
+done
